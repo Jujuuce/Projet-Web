584d1b2ce8e815ae5263b8c08cd22a52de17ba93
Jujuuce <120312333+Jujuuce@users.noreply.github.com>
2024-04-03 11:47:02 +0200

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+$_SESSION = array();
+session_destroy();
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Le titre du projet</title>
+    <link rel="stylesheet" type="text/css" href="monStyle.css">
+</head>
+<body>
+    <h1>Se connecter</h1>
+    <form>
+    </form>
+</body>
+</html>
