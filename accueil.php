a7e2469fd2d176cc31922a14e4016803549b0ca0
JFevil <43685685+JFevil@users.noreply.github.com>
2024-04-10 12:59:01 +0200

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,17 @@
+<?php
+   session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Projet</title>
+    <script type="text/javascript" src="idCheck.js"></script>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <p>Ceci est un test</p>
+</body>
+</html>
