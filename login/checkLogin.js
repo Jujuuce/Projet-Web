ef8f42e65dbdd6ca4ee28d0251a3a93cbd3cd59a
JFevil <43685685+JFevil@users.noreply.github.com>
2024-04-10 12:52:16 +0200

    Add files via upload

--- login/checkLogin.js
+++ login/checkLogin.js
@@ -1,21 +1,35 @@
-
 function loginVerification() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    console.log("Username:", username);
-    console.log("Password:", password);
 
-    fetch('https://exemple.com/sauvegarder', {
-        method: 'POST', headers: {'Content-Type': 'application/json',},
-        body: JSON.stringify({ username: username, password: password, accept: false}),
+    // Check if username or password is empty
+    if (username === '' || password === '') {
+        console.error('Username and password are required.');
+        return;
+    }
+
+    fetch('checkLogin.php', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({username: username, password: password})
     })
     .then(response => {
-        
+        if (!response.ok) {
+            throw new Error('Erreur HTTP, statut : ' + response.status);
+        }
+        return response.json();
     })
-    .catch(error => {
-        console.error('Erreur :', error);
-    });
-
+    .then(data => {
+        if (data.success) {
+            // Authentification réussie
+            console.log(data.message);
+            // Rediriger ou effectuer d'autres actions pour les utilisateurs authentifiés
+        } else {
+            // Échec de l'authentification
+            console.error(data.message);
+            // Afficher un message d'erreur à l'utilisateur
+        }
+    })
 }
 
 window.onload = function() {
