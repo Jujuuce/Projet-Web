9be0dfd319c31fc54d9519b2545dbf1727d482b7
JFevil <43685685+JFevil@users.noreply.github.com>
2024-04-12 15:28:19 +0200

    Add files via upload

--- login/checkLogin.php
+++ login/checkLogin.php
@@ -4,9 +4,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $data->username;
     $password = $data->password;
 
+    // Connexion à la base de données
+    $dsn = 'mysql:host=localhost;dbname=dataBase_projet';
+    $db_username = 'root';
+    $db_password = '';
+
+    try {
+        $bdd = new PDO($dsn, $db_username, $db_password);
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        $response['success'] = false;
+        $response['message'] = 'Erreur de connexion : ' . $e->getMessage();
+        echo json_encode($response);
+        exit(); // Arrêter l'exécution du script en cas d'erreur de connexion
+    }
+
+    // Exemple de requête pour récupérer toutes les données d'une table
+    $requete = $bdd->prepare('SELECT * FROM `Users` WHERE Users.login = :a AND Users.password = :b');
+    $requete->execute(array('a' => $username, 'b' => $password));
+    $resultats = $requete->fetchAll(PDO::FETCH_ASSOC);
+
     $response = array();
-    $response['success'] = true;
-    $response['message'] = $username." ".$password;
+    
+    // Affichage des résultats
+    if (count($resultats) == 1) {
+        $response['success'] = true;
+        $response['message'] = 'Connexion reussie';
+    } else {
+        $response["success"] = false;
+        $response["message"] = "Connexion échouée";
+    }
+    
     echo json_encode($response);
 }
-?>
