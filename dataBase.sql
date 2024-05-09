609cb249d641bf7f684b1e639394b66fdadd8a1a
Jujuuce <120312333+Jujuuce@users.noreply.github.com>
2024-05-09 19:14:22 +0200

    Add files via upload

--- dataBase.sql
+++ dataBase.sql
@@ -1,15 +1,9 @@
 CREATE TABLE `Users` (
-  `login` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL PRIMARY KEY,
   `password` varchar(255) NOT NULL,
-  `connected` int(11) NOT NULL DEFAULT 0
+  `connected` int(11) NOT NULL DEFAULT 0,
+  X int(11) NOT NULL DEFAULT 0,
+  Y int(11) NOT NULL DEFAULT 0,
+  orientation varchar(255) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `Users` (`login`, `password`, `connected`) VALUES
-('user1', '123456789', 0),
-('user2', '123456789', 0),
-('user3', '123456789', 0),
-('user4', '123456789', 0);
-
-ALTER TABLE `Users`
-  ADD PRIMARY KEY (`login`);
-COMMIT;
