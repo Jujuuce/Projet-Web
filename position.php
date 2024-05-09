32f29547a83f68ba3849e1debbb379c6afb0f90e
Jujuuce <120312333+Jujuuce@users.noreply.github.com>
2024-05-09 17:15:13 +0200

    Add files via upload

--- position.php
+++ position.php
@@ -43,3 +43,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     echo json_encode($response);
 }
+
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $data = json_decode(file_get_contents('php://input'));
+
+    // Connexion à la base de données
+    $dsn = 'mysql:host=localhost;dbname=dataBase_projet';
+    $db_username = 'root';
+    $db_password = '';
+    $response = array();
+
+    try {
+        $bdd = new PDO($dsn, $db_username, $db_password);
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        $response['users'] = false;
+        $response['success'] = false;
+        $response['message'] = 'Erreur : ' . $e->getMessage();
+        echo json_encode($response);
+        exit(); // Arrêter l'exécution du script en cas d'erreur de connexion
+    }
+
+    try {
+        $requete = $bdd->prepare('SELECT * FROM `Users` WHERE Users.connected = :a');
+        $requete->execute(array('a' => 1));
+        $resultats = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+        $positions = array();
+
+        for ($i = 0; $i < count($resultats); $i++) {
+            $pos = $resultats[$i];
+            $user = $pos['login'];
+            $x = $pos['X'];
+            $y = $pos['Y'];
+            $positions[$i] = [$user, $x, $y];
+        }
+        
+        $response['users'] = $positions;
+        $response['success'] = true;
+        $response['message'] = 'ok';
+    } catch (PDOException $e) {
+        $response['users'] = false;
+        $response['success'] = false;
+        $response['message'] = 'Erreur : ' . $e->getMessage();
+        echo json_encode($response);
+        exit(); // Arrêter l'exécution du script en cas d'erreur de connexion
+    }
+
+    echo json_encode($response);
+
+}
