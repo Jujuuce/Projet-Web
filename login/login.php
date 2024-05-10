f93e36e294036fade748d4cfc4db6a6f1557b160
JFevil <43685685+JFevil@users.noreply.github.com>
2024-05-10 23:27:22 +0200

    Add files via upload

---
+++ login/login.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+    if(isset($_SESSION['username'])){
+        header('Location: ../accueil.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>JJ Town - Connexion</title>
+	<link href="../style.css" rel="stylesheet" >
+	<script defer type="text/javascript" src="checkLogin.js" defer></script>
+</head>
+<body>
+    <h1 class="formTitle">Connexion</h1>
+    <div id="container">
+        <form id="loginForm">
+            <label for="username">Pseudo :</label>
+            <input type="text" id="username" name="username"/>
+            <label for="password">Mot de passe :</label>
+            <input type="password" id="password" name="password"/>
+            <input id="submitBouton" type="submit"/>
+            <div id="errorMessage"></div>
+        </form>
+        <a href="../signUp/signUpPage.html">Pas encore inscrit ?</a>
+    </div>
+</body>
+</html>
