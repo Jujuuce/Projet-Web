5b7ffd8a9094687ea48d891d1d0136d1f6ee8072
JFevil <43685685+JFevil@users.noreply.github.com>
2024-05-11 01:37:10 +0200

    Update dataBase.sql

--- dataBase.sql
+++ dataBase.sql
@@ -1,17 +1,82 @@
-CREATE TABLE `Users` (
-  `login` varchar(255) NOT NULL PRIMARY KEY,
-  `password` varchar(255) NOT NULL,
-  `connected` int(11) NOT NULL DEFAULT 0,
-  X int(11) NOT NULL DEFAULT 0,
-  Y int(11) NOT NULL DEFAULT 0,
-  orientation varchar(255) NOT NULL,
-  lastConnection int(11) NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : sam. 11 mai 2024 à 01:36
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `dataBase_projet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
 
 CREATE TABLE `messages` (
-  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user` varchar(255) NOT NULL,
   `heure` varchar(255) NOT NULL,
   `mess` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Users`
+--
+
+CREATE TABLE `Users` (
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `connected` int(11) NOT NULL DEFAULT 0,
+  `X` int(11) NOT NULL DEFAULT 0,
+  `Y` int(11) NOT NULL DEFAULT 0,
+  `orientation` varchar(255) NOT NULL,
+  `lastConnection` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`login`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
