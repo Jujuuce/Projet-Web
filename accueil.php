3a6edcc28adcda448f05da95d65122b388ec8c06
Jujuuce <120312333+Jujuuce@users.noreply.github.com>
2024-05-12 17:56:04 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -36,7 +36,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>100x100 Grid</title>
+    <title>JJ Town</title>
     <link rel="stylesheet" href="style.css">
     <script defer src="map.js"></script>
 </head>
