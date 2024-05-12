8d0ee52388aabd4f47babd5c56e8419756761e1d
Jujuuce <120312333+Jujuuce@users.noreply.github.com>
2024-05-12 16:16:48 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -27,25 +27,6 @@
 
         unset($_SESSION['login']);
     }
-
-    if (isset($_POST['delete'])) {
-        $dsn = 'mysql:host=localhost;dbname=dataBase_projet';
-        $db_username = 'root';
-        $db_password = '';
-    
-        try {
-            $bdd = new PDO($dsn, $db_username, $db_password);
-            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            exit(); // Arrêter l'exécution du script en cas d'erreur de connexion
-        }
-
-        $requete = $bdd->prepare('DELETE FROM Users WHERE Users.login = :a');
-        $requete->execute(array('a' => $_SESSION['login']));
-        $resultats = $requete->fetchAll(PDO::FETCH_ASSOC);
-
-        unset($_SESSION['login']);
-    }
 ?>
 
 
@@ -71,11 +52,7 @@
                         <input type="submit" name="logout" value="Déconnexion">
                     </form>
                 <?php endif; ?>
-                <?php if(isset($_SESSION['login'])): ?>
-                    <form method="post" class="formButton">
-                        <input type="submit" name="delete" value="Réglages">
-                    </form>
-                <?php endif; ?>
+                <a href="reglage/reglage.php"><button>Réglages</button></a>
             </div>
 
             <div id="messageBox">
