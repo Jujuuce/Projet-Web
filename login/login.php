b29d8eca4767e81d39744b8b089bc17186eee9df
Jujuuce <120312333+Jujuuce@users.noreply.github.com>
2024-05-15 14:38:48 +0200

    Update login.php

--- login/login.php
+++ login/login.php
@@ -10,23 +10,23 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>JJ Town - Connexion</title>
 	<link href="../style.css" rel="stylesheet" >
-	<script defer type="text/javascript" src="checkLogin.js" defer></script>
+	<script defer src="checkLogin.js"></script>
 </head>
 <body>
     <h1 class="formTitle">Connexion</h1>
     <div id="container">
         <form id="loginForm">
             <label for="username">Pseudo :</label>
-            <input type="text" id="username" name="username"/>
+            <input type="text" id="username" name="username">
             <label for="password">Mot de passe :</label>
-            <input type="password" id="password" name="password"/>
-            <input id="submitBouton" type="submit"/>
+            <input type="password" id="password" name="password">
+            <input id="submitBouton" type="submit">
             <div id="errorMessage"></div>
         </form>
         <a href="../signUp/signUpPage.php">Pas encore inscrit ?</a>
