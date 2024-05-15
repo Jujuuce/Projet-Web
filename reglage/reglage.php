9f5258d5a5049f2c5b9a3a4a4cdbf0d2d8be4ac6
Jujuuce <120312333+Jujuuce@users.noreply.github.com>
2024-05-15 20:12:12 +0200

    Update reglage.php

--- reglage/reglage.php
+++ reglage/reglage.php
@@ -35,6 +35,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paramètres</title>
     <link rel="stylesheet" href="../style.css">
+    <link rel="shortcut icon" type="image/png" href="../images/favicon.png"/>
 </head>
 <body>
     <h1 class="formTitle">Paramètres</h1>
