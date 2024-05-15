10b8b3280cc7c119362cbf844ed9fc7a6e4d7d3e
Jujuuce <120312333+Jujuuce@users.noreply.github.com>
2024-05-15 14:27:55 +0200

    Update changermdp.php

--- reglage/changermdp.php
+++ reglage/changermdp.php
@@ -21,17 +21,17 @@
     <h1 class="formTitle">Changer le mot de passe</h1>
     <div id="container">
         <div class="retour">
-            <a href="reglage.php"><img src="../fleche_retour.png" /></a>
+            <a href="reglage.php"><img alt="Retour" src="../fleche_retour.png"></a>
         </div>
         <div class="quitter">
-            <a href="../accueil.php"><img src="../croix.png" /></a>
+            <a href="../accueil.php"><img alt="Retour à l'accueil" src="../croix.png"></a>
         </div>
         <form id="newPass">
             <label for="pass">Nouveau mot de passe :</label>
-            <input type="password" id="pass" name="pass"/>
+            <input type="password" id="pass" name="pass">
             <label for="pass1">Confirmer nouveau mot de passe :</label>
-            <input type="password" id="pass1" name="pass1"/>
-            <input id="submitBouton" type="submit"/>
+            <input type="password" id="pass1" name="pass1">
+            <input id="submitBouton" type="submit">
             <div id="errorMessage"></div>
         </form>
     </>
