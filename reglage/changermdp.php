c750a345a1c726f63274187092d559878114f274
Jujuuce <120312333+Jujuuce@users.noreply.github.com>
2024-05-15 20:12:39 +0200

    Update changermdp.php

--- reglage/changermdp.php
+++ reglage/changermdp.php
@@ -15,6 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chnager le mot de passe</title>
     <link rel="stylesheet" href="../style.css">
+    <link rel="shortcut icon" type="image/png" href="../images/favicon.png"/>
     <script defer src="mdp.js"></script>
 </head>
 <body>
