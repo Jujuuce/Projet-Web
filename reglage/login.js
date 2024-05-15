bc609a3be042a64311d8111eb35257b8074966ab
JFevil <43685685+JFevil@users.noreply.github.com>
2024-05-15 18:47:46 +0200

    Update login.js

--- reglage/login.js
+++ reglage/login.js
@@ -1,29 +1,3 @@
-function checkID() {
-    fetch("../idCheck.php", { 
-        method: 'GET',
-        headers: {'Content-Type': 'application/json;charset=utf-8'}
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Erreur lors de la requête Fetch.");
-        }
-        return response.json();
-    })
-    .then(content => {
-        if (content["session"] == 0) {
-            window.location.replace("../login/login.php");
-        }
-    })
-    .catch(error => {
-        console.error('Erreur lors de la récupération des données :', error);
-    });
-}
-
-checkID();
-
-
-
-
 function newLogin() {
 
     var login = document.getElementById("login").value;
@@ -43,7 +17,7 @@ function newLogin() {
     })
     .then(data => {
         if (data.success) {
-            window.location.replace("../accueil.php");
+            window.location.replace("../index.php");
         } else {
             messageBox.innerHTML = data.message;
         }
