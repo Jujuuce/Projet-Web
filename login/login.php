b766b5d799e3973648a83d7f5fc3c34fe4d759d3
Jujuuce <120312333+Jujuuce@users.noreply.github.com>
2024-05-15 20:16:48 +0200

    Update login.php

--- login/login.php
+++ login/login.php
@@ -10,25 +10,25 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>JJ Town - Connexion</title>
 	<link href="../style.css" rel="stylesheet" >
-	<link rel="shortcut icon" type="image/png" href="../images/favicon.png"/>
-	<script defer type="text/javascript" src="checkLogin.js" defer></script>
+	<link rel="shortcut icon" type="image/png" href="../images/favicon.png">
+	<script defer type="text/javascript" src="checkLogin.js"></script>
 </head>
 <body>
     <h1 class="formTitle">Connexion</h1>
     <div id="container">
         <form id="loginForm">
             <label for="username">Pseudo :</label>
-            <input type="text" id="username" name="username"/>
+            <input type="text" id="username" name="username">
             <label for="password">Mot de passe :</label>
-            <input type="password" id="password" name="password"/>
+            <input type="password" id="password" name="password">
             <div id="errorMessage"></div>
-            <input id="submitBouton" type="submit"/>
+            <input id="submitBouton" type="submit">
         </form>
         <a href="../signUp/signUpPage.php">Pas encore inscrit ?</a>
     </div>
